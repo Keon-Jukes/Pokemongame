0150f65e8b7a19406e6cd4286240edff2d4b8b40
keonjukes <keonofthephanerozoiceon@gmail.com>
2022-06-08 16:33:01 -0400

    nicer images added

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,20 +1,24 @@
 import React from "react"
 import "./Pokecard.css";
 
-const PokeAPI = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const PokeAPI = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3): number);
 
 class Pokecard extends React.Component {
     
     render(){
       const pokemonObject = this.props.pokemonObject;
-      let imgSrc= `${PokeAPI}${pokemonObject.id}.png`
+      let imgSrc= `${POKE_API}${padToThree(pokemonObject.id)}.png`
     //   console.log('this is pokemonObject --->', this.props);
         return(<div>
           <div className="Pokecard">
-            <h1>{pokemonObject.name}</h1>
+            <h1 className="Pokecard-title">{pokemonObject.name}</h1>
             <img src={imgSrc} alt={this.props.name}/>
-            <p>Type:{pokemonObject.type}</p>
-            <p>EXP: {pokemonObject.base_experience}</p>
+            <p className="Pokecard-data">Type:{pokemonObject.type}</p>
+            <p className="Pokecard-data">EXP: {pokemonObject.base_experience}</p>
             </div>
         </div>)
     }
